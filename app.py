d8726ca685d914a73db8b845b4b39222ea6fe5bf
Justus <justus.d2025@gmail.com>
2025-07-09 23:45:21 +0200

    duh

--- app.py
+++ app.py
@@ -1,46 +1,59 @@
 from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
-
-from data_models import db, Author, Book
+import os
+from data_models import db, Book, Author
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite'
+# OperationError fix
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data', 'library.sqlite')
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data/library.sqlite' 
+
+
 
 db.init_app(app)
-
+    
+with app.app_context():
+    db.create_all()
 @app.route('/')
 def index():
     # Example: Fetch all books from the database
+    
+    print(Author.query.all())
+    print(Author.query.all()[Book.query.all()[0].author_id].name)
     books = Book.query.all()
-    return render_template('index.html', books=books)
+    return render_template('home.html', books=books,authors = Author.query.all())
 
-@app.route('/add', methods=['GET', 'POST'])
+
+@app.route('/add_author', methods=['GET', 'POST'])
+def add_author():
+    if request.method == 'POST':
+        name = request.form['name']
+        birth_date = request.form['birth_date']
+        date_of_death = request.form['date_of_death']
+        new_author = Author(name = name, birth_date = birth_date, date_of_death = date_of_death)
+        db.session.add(new_author)
+        db.session.commit()
+        return redirect('/')
+    return render_template('add_author.html')
+
+@app.route('/add_book', methods=['GET', 'POST'])
 def add_book():
     if request.method == 'POST':
+        
         title = request.form['title']
         author = request.form['author']
-        year = request.form['year']
-        new_book = Book(title=title, author=author, year=year)
+        isbn = request.form['isbn']
+        publication_year = request.form['publication_year']
+        new_book = Book(title=title, author_id=author, publication_year=publication_year,isbn=isbn)
         db.session.add(new_book)
         db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('add_book.html')
+        return redirect('/')
+    return render_template('add_book.html',authors=Author.query.all())
 
-@app.route('/edit/<int:book_id>', methods=['GET', 'POST'])
-def edit_book(book_id):
-    book = Book.query.get_or_404(book_id)
-    if request.method == 'POST':
-        book.title = request.form['title']
-        book.author = request.form['author']
-        book.year = request.form['year']
-        db.session.commit()
-        return redirect(url_for('index'))
-    return render_template('edit_book.html', book=book)
+if __name__ == '__main__':
 
-@app.route('/delete/<int:book_id>', methods=['POST'])
-def delete_book(book_id):
-    book = Book.query.get_or_404(book_id)
-    db.session.delete(book)
-    db.session.commit()
-    return redirect(url_for('index'))
+    app.run(debug=True)
+    pass
--- data_models.py
+++ data_models.py
@@ -4,7 +4,13 @@ db = SQLAlchemy()
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    isbn = db.Column(db.String(100)) # ISBN can be 10 or 13 digits longs
+    title = db.Column(db.String(100))
+    publication_year = db.Column(db.Integer)
+    author_id = db.Column(db.Integer)
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     birth_date = db.Column(db.String(100), nullable=False)
     date_of_death = db.Column(db.String(100))
-Book()
+    
